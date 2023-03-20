93c83548975891a4aac0adc636723af6bf22dd68
Devon Lee <devonlee111@gmail.com>
2023-03-20 00:38:24 -0700

    Classy Waifu

--- src/waifu.py
+++ src/waifu.py
@@ -7,54 +7,61 @@ import transcriber
 OPENAI_API_KEY_FILE = "./openai_key"
 WAVE_OUTPUT_FILE = "output.wav"
 
-voice_recorder = None
-gpt_client = None
-whisper_client = None
-
-def load_waifu():
-	whisper_client = transcriber.whisper_client()
-	whisper_client.load("base")
-
-	gpt_client = chat.gpt_client()
-	gpt_client.load_api_key(OPENAI_API_KEY_FILE)
-
-	voice_recorder = recorder.recorder(WAVE_OUTPUT_FILE)
-
-def run_chat_pipeline():
-	prompt = ""
-	try:
-		prompt = whisper_client.transcribe(WAVE_OUTPUT_FILE)
-	except err:
-		print(err)
+class waifu():
+	def __init__(self):
+		self.voice_recorder = None
+		self.gpt_client = None
+		self.whisper_client = None
+		self.listener = None
 		return
 
-	try:
-		gpt_client.chat(prompt)
-	except err:
-		print (err)
-		return
 
-	# TODO integrate voice synthesis when added
+	def load_waifu(self):
+		self.whisper_client = transcriber.whisper_client()
+		self.whisper_client.load("base")
 
-def run():
-	print("Press and hold the 'r' key to begin recording")
-	print("Release the 'r' key to end recording")
-	listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-	listener.join()
+		self.gpt_client = chat.gpt_client()
+		self.gpt_client.load_api_key(OPENAI_API_KEY_FILE)
 
-def on_press(self, key):
-	if key.char == 'r':
-		voice_recorder.start()
-	return True
+		self.voice_recorder = recorder.recorder(WAVE_OUTPUT_FILE)
 
-def on_release(self, key):
-	if key.char == 'r':
-		voice_recorder.stop()
-		run_chat_pipeline()
+	def run_chat_pipeline(self):
+		prompt = ""
+		try:
+			prompt = self.whisper_client.transcribe(WAVE_OUTPUT_FILE)
+		except err:
+			print(err)
+			return
+
+		try:
+			self.gpt_client.chat(prompt)
+		except err:
+			print (err)
+			return
+
+		# TODO integrate voice synthesis when added
+
+	def on_press(self, key):
+		if key.char == 'r':
+			self.voice_recorder.start()
 		return True
-	# Any other key ends the program
-	return False
+
+	def on_release(self, key):
+		if key.char == 'r':
+			self.voice_recorder.stop()
+			self.run_chat_pipeline()
+			return True
+		# Any other key ends the program
+		return False
+
+	def run(self):
+		print("Press and hold the 'r' key to begin recording")
+		print("Release the 'r' key to end recording")
+		self.listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+		self.listener.start()
+		self.listener.join()
 
 if __name__ == "__main__":
-	load_waifu()
-	run()
+	waifu = waifu()
+	waifu.load_waifu()
+	waifu.run()
